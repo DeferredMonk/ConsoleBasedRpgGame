fa50a29ae59c2ef29692e3caccc46a75cbe783ec
DeferredMonk <marcoangeli151@gmail.com>
2023-02-08 15:19:52 +0200

    AppendixA complete

---
+++ ConsoleBasedRpgGame/Hero/CharacterRoles/Mage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedRpgGame.Hero.CharacterRoles
+{
+    public class Mage : Hero
+    {
+        private Attribute onLevelUp = new(1, 1, 5);
+        public Mage(string name) : base(name)
+        {
+            LevelAttribute = new(1, 1, 8);
+        }
+        public override void LevelUp()
+        {
+            base.LevelUp();
+            this.LevelAttribute += this.onLevelUp;
+        }
+    }
+}
+
---
+++ ConsoleBasedRpgGame/Hero/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBasedRpgGame.Hero
+{
+    public abstract class Hero
+    {
+        private int level = 1;
+        protected string[] equipment;
+        protected string[] validWeaponTypes;
+        protected string[] ValidArmorTypes;
+
+        public Attribute LevelAttribute { get; set; }
+        public int Level { get { return level; }}
+        public string? Name { get; set; }
+
+        public Hero(string name)
+        {
+            this.Name = name;  
+        }
+        public virtual void LevelUp()
+        {
+            this.level++;
+        }
+        public void EquipW(string weapon)
+        {
+
+        }
+        public void EquipA(string armor)
+        {
+
+        }
+        public void Damage()
+        {
+
+        }
+        public void TotalAttributes() 
+        { 
+
+        }
+        public void Display()
+        {
+
+        }
+
+    }
+}
